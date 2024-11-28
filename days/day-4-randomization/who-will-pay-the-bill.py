e88ba3577d8291f52a53ce75f419a1501a2122a8
Jeremaiah Mata <129203757+jeremaiiiahmata@users.noreply.github.com>
2024-11-28 20:30:44 +0800

    Add Day 4

---
+++ days/day-4-randomization/who-will-pay-the-bill.py
@@ -0,0 +1,17 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emmanuel"]
+
+chosenFriend = random.choice(friends)
+
+print(chosenFriend)
+
+#random_index = random.randint(0, 4)
+#print(friends[random_index])
+
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+
+characters = [numbers, letters]
+
+print(characters[0][0])

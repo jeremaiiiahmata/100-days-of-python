d1e814914953449cf390d2b1a289b2a424bb39a5
Jeremaiah Mata <129203757+jeremaiiiahmata@users.noreply.github.com>
2024-12-11 23:54:26 +0800

    Initial Commit

--- intermediate/day-20-and-21-snake-game/main.py
+++ intermediate/day-20-and-21-snake-game/main.py
@@ -1,60 +1,31 @@
+
 from turtle import *
+from snake import Snake
 import time
+
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game")
 screen.tracer(0)
-startingPos = [(0, 0), (-20, 0), (-40, 0)]
-snake = []
-x = 0
 
-
-for pos in startingPos:
-    block = Turtle("square")
-    block.color("white")
-    block.penup()
-
-    block.goto(pos)
-
-    snake.append(block)
-
-def moveUp():
-    snake[0].setheading(90)
-
-def moveLeft():
-    snake[0].setheading(180)
-
-def moveDown():
-    snake[0].setheading(270)
-
-def moveRight():
-    snake[0].setheading(0)
-
-
+snake = Snake()
 
 
 while True:
     screen.update()
-    time.sleep(0.1) #Waits for 0.1 seconds before updating again.
+    time.sleep(0.1) #Screens update every 0.1 seconds
 
-    for each in range(len(snake)-1, 0, -1): #Sets the position of the next block to the current block, so it will be able to follow
-        x = snake[each-1].xcor()
-        y = snake[each-1].ycor()
-        snake[each].setpos(x,y)
+    snake.move() #Everytime the screen refreshes, the snake move by 20 steps.
 
     #----- Only executes when triggered
     screen.listen() #Listens for any key events
-    screen.onkey(key="w", fun=moveUp)
-    screen.onkey(key="a", fun=moveLeft)
-    screen.onkey(key="s", fun=moveDown)
-    screen.onkey(key="d", fun=moveRight)
-
-    snake[0].forward(20) #The front block of the snake moves forward by 20 blocks.
-    #The consecutive blocks will follow the first block because of the for loop earlier (where the position of the block next to it will be the its new position)
-
-
+    screen.onkey(key="w", fun=snake.moveUp)
+    screen.onkey(key="a", fun=snake.moveLeft)
+    screen.onkey(key="s", fun=snake.moveDown)
+    screen.onkey(key="d", fun=snake.moveRight)
 
 
 screen.exitonclick()

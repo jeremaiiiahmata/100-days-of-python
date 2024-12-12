9f8ba7248b27e25976ca46574d7e4108295800ab
Jeremaiah Mata <129203757+jeremaiiiahmata@users.noreply.github.com>
2024-12-13 02:13:12 +0800

    Finish Day 25

--- intermediate/day-25-working-with-csv/50-states-game/main.py
+++ intermediate/day-25-working-with-csv/50-states-game/main.py
@@ -1,13 +1,27 @@
-from turtle import Screen
+from os import error
+from turtle import Screen, Turtle
+import pandas
 
 screen = Screen()
 image = "blank_states_img.gif"
 screen.bgpic(image)
+data = pandas.read_csv("./50_states.csv")
+states = data["state"].to_list()
 
-def getMouseClick(x, y):
-    answer = screen.textinput(title="Enter State : " )
-    print(x, y)
+guessedStates = []
 
-screen.onscreenclick(getMouseClick)
+while len(guessedStates) < 50 :
+    answer = screen.textinput(title=f"{len(guessedStates)}/50 States", prompt="Enter a state name :" )
 
-screen.mainloop()
+    if answer.title() in states:
+        #Creates a turtle with the name on the x,y coordinates of the state
+        coordinates = data[data["state"] == answer.title()]
+        turtle = Turtle()
+        turtle.penup()
+        turtle.hideturtle()
+        turtle.goto(coordinates.x.item(), coordinates.y.item())
+        turtle.write(answer.title())
+
+        guessedStates.append(answer)
+
+screen.exitonclick()

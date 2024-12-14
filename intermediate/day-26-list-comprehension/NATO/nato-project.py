3aab96573a886d9d6fa4498f27ca09686f88ac60
Jeremaiah Mata <129203757+jeremaiiiahmata@users.noreply.github.com>
2024-12-14 23:52:34 +0800

    Add comments to day 26

--- intermediate/day-26-list-comprehension/NATO/nato-project.py
+++ intermediate/day-26-list-comprehension/NATO/nato-project.py
@@ -1,12 +1,19 @@
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
+data = pandas.read_csv("nato_phonetic_alphabet.csv") #Reads the file in .csv format, one of the features of pandas
+print(data)
+#Using a dict comprehension, for each index and row in the data, create a new key and value
+#New key = row.letter & New value = row.code
+#data.iterrows() -> loops through each row of the .csv
+#(index, rows) unpacks the  key-value pairs
+#index -> built-in variable and can't be changed
+#rows -> a pandas series object that represents the data of the current row.
 natoDictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 print(natoDictionary)
 
 message = input("Enter a message : ")
 
+#Using a list comprehension,
 natoMessage = [natoDictionary[letter.upper()] for letter in message]
 
 print(natoMessage)
--- intermediate/day-26-list-comprehension/dict-comp.py
+++ intermediate/day-26-list-comprehension/dict-comp.py
@@ -4,9 +4,10 @@ import pandas
 names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
 
 #---- Creating new dictionary from a list
-studentsScores = {student:random.randint(1,100) for student in names}
-print(studentsScores)
+studentsScores = {student:random.randint(1,100) for student in names} #Gives each student a random grade
+print(f"Student scores :{studentsScores}")
 
+#Loops through each row of the studentsScore and creates a new dictionary for those whose grades are greater than 75
 passedStudents = {student : score for (student, score) in studentsScores.items() if score > 75}
 print(passedStudents)
 
